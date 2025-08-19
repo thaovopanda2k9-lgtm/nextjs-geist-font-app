a62ba760c6fa4aeb68a6cf29949303698b994e3c
thaovopanda2k9-lgtm <thaovopanda2k9@gmail.com>
2025-08-19 17:41:51 +0700

    Add .next/static/chunks/src_app_voice-detection_page_tsx_a993527f._.js 1755600109911

---
+++ .next/static/chunks/src_app_voice-detection_page_tsx_a993527f._.js
@@ -0,0 +1,7 @@
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/_4ae8a718._.js"
+],
+    source: "dynamic"
+});
